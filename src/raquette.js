67567002686977f0c755373b109f4a4269fc9f65
Thomas <thomas.fontaneau@etudiant.univ-lr.fr>
2022-03-24 16:42:19 +0100

    ajout de la prise en charge de levent fleche droite

--- src/raquette.js
+++ src/raquette.js
@@ -1,30 +1,62 @@
 /*Raquette*/
-let raquetteX = 300;
-let raquetteY = 700;
-
-let raquetteLarge = 200;
-let raquetteHaute = 25;
-
 
 //https://developer.mozilla.org/fr/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Paddle_and_keyboard_controls
 
 
-function drawRaquette(ctx,rightPressed,leftPressed)
+class Raquette
 {
-    ctx.clear;
-
-    console.log(rightPressed)
-
-    if(rightPressed==true)
+    constructor(positionX,positionY)
     {
-        raquetteX=raquetteX-5;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.large = 200;
+        this.hauteur = 25;
+        this.vitesse = 5;
     }
 
-    console.log(raquetteX);
-    ctx.rect(raquetteX, raquetteY,raquetteLarge , raquetteHaute);
-    ctx.fillStyle = "orange";
+    drawRaquette(ctx)
+    {
+        console.log("Ajout de la raquette au jeu");
+        ctx.rect(this.positionX,this.positionY, this.large , this.hauteur);
+        ctx.fillStyle = "orange";
+    
+        ctx.fill();
+    }
 
-    ctx.fill();
+    deleteRaquette(ctx)
+    {
+        console.log("La raquette est supprimer");
+        ctx.clearRect(this.positionX,this.positionY,this.large,this.hauteur);
+    }
+
+    getpositionX()
+    {
+        return this.positionX;
+    }
+
+    setpositionX(positionX)
+    {
+        this.positionX = positionX;
+    }
+
+    getpositionY()
+    {
+        return this.positionY;
+    }
+
+    setpositionY(positionY)
+    {
+        this.positionY = positionY;
+    }
+
+    getvitesse()
+    {
+        return this.vitesse;
+    }
+
+    setvitesse(vitesse)
+    {
+        this.vitesse = vitesse;
+    }
+    
 }
-
-/* */
--- src/source.js
+++ src/source.js
@@ -7,58 +7,13 @@ let y;
 let rightPressed = false;
 let leftPressed = false;
 
-function mouseMove(event)
-{
-    getMousePos(event);
-
-    // Si started == true, alors tracer une ligne
-    if(started)
-    {
-        context.lineTo(x, y);
-        // Dessine la forme
-        context.stroke();
-    }
-}
-
-function mouseClick(event)
-{
-    // Lorsque le clic est detecte, passe la variable started à true
-    // et deplace la position initiale de la souris
-    context.beginPath();
-    context.moveTo(x, y);
-    started = true;
-}
-
-function mouseUp(event)
-{
-    // Passer la variable started a false lorsque le bouton est relache
-    if(started)
-    started = false;
-}
-
-function getMousePos(evt)
-{
-    let rect = canvas.getBoundingClientRect();
-    let scaleX = canvas.width / rect.width;
-    let scaleY = canvas.height / rect.height;
-    x = (evt.clientX - rect.left) * scaleX;
-    y = (evt.clientY - rect.top) * scaleY;
-}
 
 function clearCanvas(event)
 {
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function keyDownHandler(event)
-{
-    if(event.key == "Right" || event.key == "ArrowRight")
-    {
-        rightPressed=true;
-        console.log(rightPressed);
-        drawRaquette(context,rightPressed,leftPressed);
-    }
-}
+
 
 $(document).ready(function()
 {
@@ -68,15 +23,26 @@ $(document).ready(function()
     context.lineWidth = 1;
     context.strokeStyle = "#871de0";
 
-    // Ajout des gestionnaires d'evenements
-    canvas.addEventListener('mousemove', mouseMove);
-    canvas.addEventListener('mousedown', mouseClick);
-    canvas.addEventListener('mouseup', mouseUp);
+    //Raquette
 
-    document.addEventListener("keydown", keyDownHandler, false);
+     let raquette = new Raquette(200,200);
+    raquette.drawRaquette(context);
+
+    document.addEventListener("keydown", keyDownHandler);
     //document.addEventListener("keyup", keyUpHandler, false);
 
-    drawRaquette(context,rightPressed,leftPressed);
+    console.log(raquette.getpositionX());
+   
 
+    function keyDownHandler(event)
+    {
+        if(event.key == "Right" || event.key == "ArrowRight")
+        {
+            raquette.deleteRaquette(context);
+            raquette.setpositionX(raquette.getpositionX()+raquette.getvitesse())
+            raquette.drawRaquette(context);
+        }
+
+    }
 });
 
