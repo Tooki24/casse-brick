3c4a72e07fab04b4ab9dee3f2fbc3f3f926d0653
SodaMosa <80697061+SodaMosa@users.noreply.github.com>
2022-03-29 10:00:09 +0200

    fix/update: refonte de la classe brick et gestion de l'appartion de briques normales et vitesses

--- src/brick.js
+++ src/brick.js
@@ -1,70 +1,117 @@
 //classe brick
 class Brick{
-    constructor(type,points,img){
+    constructor(longueur, largeur, type, x, y, points){
+    this.long=longueur;
+    this.larg=largeur;
     this.type=type;
+    this.positionX=x;
+    this.positionY=y;
     this.points=points;
-    this.img=new Image();
-    this.img.src=img
-    this.long=40/1.5
-    this.larg=112/1.5
+    console.log(this.positionX)
     }
-}
 
-//génère un nombre random de 0 a max
-function getRandom(max){
-    return Math.floor(Math.random()*max)
-}
-
-//dessine un tableau de brick avec un maximum de 4 bricks vitesses
-function drawBrick(ctx,){
-    let nbSpeedmax=6
-    let nbSpeed=0
-    //génère la grille des bricks (9x6)
-    for (let larg=0;larg<9;larg++ ){
-        for (let lon=0; lon<6; lon++){
-            let normalBrick= new Brick("normal",10,'/img/normal_brick.png')
-            let speedBrick= new Brick("vitesse",30, "/img/speed_brick.png")
-            let img=normalBrick.img
-            let img2= speedBrick.img
-            let x= normalBrick.larg*larg
-            let y=normalBrick.long*lon
-            let typeBrick=getRandom(2)
-            //affiche une brick normale si le nombre aléatoire est de 0, sinon une brick vitesse si il y en a pas encore "nbSpeedmax", sinon met une brick normale
-            if(typeBrick==0){
-                normalBrick.img.onload=function(){
-                    ctx.drawImage(img,x,y,112/1.5,40/1.5)
-                
-                }
-            }
-            else if(typeBrick==1){
-                if(nbSpeed<nbSpeedmax){
-                    if(getRandom(5)==0){
-                        speedBrick.img.onload=function(){
-                            ctx.drawImage(img2,x,y,112/1.5,40/1.5)
-                        }
-                        nbSpeed+=1
-                    }
-                    else {
-                        normalBrick.img.onload=function(){
-                            ctx.drawImage(img,x,y,112/1.5,40/1.5)
-                        
-                        }
-
-
-                    }
-                }
-                else {
-                    normalBrick.img.onload=function(){
-                        ctx.drawImage(img,x,y,112/1.5,40/1.5)
-                    
-                    }
-
-
-            }
-
-
-            }
+    drawBrick(ctx) {
+        ctx.beginPath();
+        let img = new Image()
+        // lorsque type vaut 0 il s'agit d'une brique normale, lorsqu'il vaut 1 il s'agit d'une brique de vitesse
+        if(this.type == 0)
+        {
+            ctx.rect(this.positionX, this.positionY, this.long, this.larg);
+            ctx.fillStyle = "#0095DD";
         }
+        else if(this.type == 1)
+        {
+            ctx.rect(this.positionX, this.positionY, this.long, this.larg);
+            ctx.fillStyle = "#f00020";
+        }
+
+
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    getType()
+    {
+        return this.type;
+    }  
+
+    getPoints()
+    {
+        return this.points;
     }
 }
+
+
+
+
+// //dessine un tableau de brick avec un maximum de 4 bricks vitesses
+//     drawBrick(ctx, i){
+
+//     if(this.type==1)
+//     {
+//         let imgNormal = new Image();
+//         imgNormal.src = "/img/normal_brick.png"
+//         imgNormal.onload = function()
+//         {
+//             console.log("je charge l'img")
+//             ctx.drawImage(imgNormal,0,0)
+//         }
+//     }
+//     else if(this.type==3)
+//     {
+//         let imgSpeed = new Image();
+//         imgSpeed.onload = function()
+//         {
+//             ctx.drawImage("/img/speed_brick.png",)
+//         }
+//     }
+// }
+// }
+
+
+
+
+    // let nbSpeedmax=6
+    // let nbSpeed=0
+    // //génère la grille des bricks (9x6)
+    // for (let larg=0;larg<9;larg++ ){
+    //     for (let lon=0; lon<6; lon++){
+    //         let normalBrick= new Brick("normal",10,'/img/normal_brick.png')
+    //         let speedBrick= new Brick("vitesse",30, "/img/speed_brick.png")
+    //         let img=normalBrick.img
+    //         let img2= speedBrick.img
+    //         let x= normalBrick.larg*larg
+    //         let y=normalBrick.long*lon
+    //         let typeBrick=getRandom(2)
+    //         //affiche une brick normale si le nombre aléatoire est de 0, sinon une brick vitesse si il y en a pas encore "nbSpeedmax", sinon met une brick normale
+    //         if(typeBrick==0){
+    //             normalBrick.img.onload=function(){
+    //                 ctx.drawImage(img,x,y,112/1.5,40/1.5)   
+    //             }
+    //         }
+    //         else if(typeBrick==1){
+    //             if(nbSpeed<nbSpeedmax){
+    //                 if(getRandom(5)==0){
+    //                     speedBrick.img.onload=function(){
+    //                         ctx.drawImage(img2,x,y,112/1.5,40/1.5)
+    //                     }
+    //                     nbSpeed+=1
+    //                 }
+    //                 else {
+    //                     normalBrick.img.onload=function(){
+    //                         ctx.drawImage(img,x,y,112/1.5,40/1.5)
+                        
+    //                     }
+    //                 }
+    //             }
+    //             else {
+    //                 normalBrick.img.onload=function(){
+    //                     ctx.drawImage(img,x,y,112/1.5,40/1.5)
+                    
+    //                 }
+    //         }
+    //         }
+    //     }
+    // }
+
 
